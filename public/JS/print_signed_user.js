935944a5912c30e06c33543e8e03b9397d8bd338
mantashakhan <mantashakhan20092000@gmail.com>
2020-05-16 17:46:00 -0700

    public

---
+++ public/JS/print_signed_user.js
@@ -0,0 +1,11 @@
+let url="http://localhost:3000/get_signed_user";
+        fetch(url).then(response => response.json())
+        .then( (result) => {
+            console.log('success:', result)
+            username_g = result.username
+            if(result.username!=undefined){
+            	document.getElementById("loginBtn").innerHTML = "<p style='font-size:35px'>"+result.username+"</p>"
+            }
+            
+        })
+        .catch(error => console.log('error:', error));
